1166e47137c568dda5c4766be65a17d81b16ed59
Muhammed Emir TUNCER <59527491+TuncerEmir@users.noreply.github.com>
2023-06-12 00:30:57 +0300

    Add files via upload
    “Program that sorts the numbers taken from the user in ascending order using the selection sort algorithm

---
+++ SortIntArr.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void selectionSort(int array[],int size);
+void minToMax(int array[],int size);
+
+int main(){
+    int n;
+    printf("Eleman sayısını girin: ");
+    n = get_int("");
+    int array[n];
+    for (int i=0; i<n; i++) {
+        printf("%d. elemanı girin: ", i+1);
+        array[i] = get_int("");
+    }
+    
+    selectionSort(array,n);
+    printf("Sırası ile küçükten büyüğe: ");
+    for(int i =0;i<n;i++){
+        printf("%d ",array[i]);
+    }
+    printf("\n");
+}
+
+void selectionSort(int array[],int size){
+	int min;
+	
+	for(int i =0;i<size-1;i++){
+		min = i;
+		
+		for(int j = i;j<size;j++){
+			if(array[j]<array[min]){
+				min = j;
+			}
+		}
+		int temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+	}
+}
+
+void minToMax(int array[],int size){
+	int max;
+	
+	for(int i=0;i<size;i++){
+		max  = i;
+		for(int j=i;j<size;j++){
+			if(array[j]>array[max]){
+				max= j;
+			}
+		}
+		int temp = array[i];
+		array[i] = array[max];
+		array[max] = temp;
+	}
+}
